88a7f9c83081ab4259294856571f3f37736d3102
paradigm1984 <danblanco1990@gmail.com>
2017-06-06 15:00:48 -0400

    created json file, added apis, made keys.js

---
+++ keys.js
@@ -0,0 +1,49 @@
+
+// ================= TWITTER ================= //
+
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+
+  // Do something 
+});
+
+// ================= SPOTIFY ================= //
+
+var spotify = require('spotify');
+ 
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    // Do something
+});
+
+// ================= REQUEST ================= //
+
+// Use the [OMDB API](http://www.omdbapi.com).
+
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred 
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+  console.log('body:', body); // Print the HTML for the Google homepage. 
+});
+
+
+
+
+
