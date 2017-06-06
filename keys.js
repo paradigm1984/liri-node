01471f922c44693e9cf88ff68101151d0e5dd471
paradigm1984 <danblanco1990@gmail.com>
2017-06-06 17:12:45 -0400

    updating node modules and gitignore

--- keys.js
+++ keys.js
@@ -37,7 +37,7 @@ spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(er
 // Use the [OMDB API](http://www.omdbapi.com).
 
 var request = require('request');
-request('http://www.google.com', function (error, response, body) {
+request('http://www.omdbapi.com', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred 
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
   console.log('body:', body); // Print the HTML for the Google homepage. 
